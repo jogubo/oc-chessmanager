0c2db8d356620614ed4e8482465f79d2b11393b6
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2021-11-18 00:06:31 +0100

    amelioration du code pour la recherche d'un joueur

--- controllers/players_ctrl.py
+++ controllers/players_ctrl.py
@@ -10,26 +10,23 @@ class PlayerCtrl:
 
     def __call__(self):
         serialized_players = Database.get('players')
-        players = []
-        for player in serialized_players:
-            player = Player(
-                    first_name=player["first_name"],
-                    last_name=player["last_name"],
-                    birth=player["birth"],
-                    civility=player["civility"],
-                    rank=player["rank"],
-                    id=player["id"]
-                    )
-            players.append(player)
-        self._view.list(players)
+        players = self.list(serialized_players)
+        return players
 
     def search(self):
         '''
         Search a player by name
         '''
         search_result = Database.search('players', self._view.search())
+        players = self.list(search_result)
+        user_choice = self._view.list(players)
+        player = players[int(user_choice) - 1]
+        print(player.id)
+        return players
+
+    def list(self, results):
         players = []
-        for player in search_result:
+        for player in results:
             player = Player(
                     first_name=player["first_name"],
                     last_name=player["last_name"],
@@ -39,6 +36,8 @@ class PlayerCtrl:
                     id=player["id"]
                     )
             players.append(player)
-        player = self._view.list(players)
-        player = players[int(player) - 1]
-        return player.id
+        return players
+
+    @staticmethod
+    def new_player():
+        pass
