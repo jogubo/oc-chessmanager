574ccdc0d9365569191d6dbdde8f31c63aecceac
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2021-11-30 17:00:17 +0100

    ajout d'un algo pour générer les paires de joueurs pour le premier tour

--- controllers/tournament_ctrl.py
+++ controllers/tournament_ctrl.py
@@ -10,7 +10,7 @@ class TournamentCtrl:
                 data["name"],
                 "location",
                 # data["players"],
-                [[1, 3, 4], [2, 6, 12], [3, 6, 15], [6, 12, 1]],
+                [[1, 0, 4], [7, 3, 3], [10, 0, 10], [2, 0, 12], [3, 1, 15], [6, 0, 1]],
                 "date",
                 "turns",
                 "rounds",
@@ -18,21 +18,25 @@ class TournamentCtrl:
                 "description"
                 )
 
-        self._players = []
-        for player in self._tournament.players:
-            id = player[0]
-            serialized_player = Database.get('players', id)
-            player = Player(
-                    first_name=serialized_player["first_name"],
-                    last_name=serialized_player["last_name"],
-                    birth=serialized_player["birth"],
-                    civility=serialized_player["civility"],
-                    rank=serialized_player["rank"],
-                    id=serialized_player.doc_id
-                    )
-            self._players.append(player)
+        #self._players = []
+        #for player in self._tournament.players:
+        #    id = player[0]
+        #    serialized_player = Database.get('players', id)
+        #    player = Player(
+        #            first_name=serialized_player["first_name"],
+        #            last_name=serialized_player["last_name"],
+        #            birth=serialized_player["birth"],
+        #            civility=serialized_player["civility"],
+        #            rank=serialized_player["rank"],
+        #            id=serialized_player.doc_id
+        #            )
+        #    self._players.append(player)
 
     def sort_players(self):
+        '''
+        Player's data is list:
+            [player_id, player_score, player_rank]
+        '''
         players = self._tournament.players
         score, rank = 1, 2
         players.sort(key=lambda x: x[score], reverse=True)
@@ -43,8 +47,25 @@ class TournamentCtrl:
                         temp = players[j]
                         players[j] = players[j+1]
                         players[j+1] = temp
-
+        self._players = players
         return players
 
+    def generate_versus(self):
+        players = self._players
+        median = int(len(players) / 2)
+        versus = []
+        if len(players[0]) == 3:
+            for i in range(median):
+                versus.append((players[i], players[i+median]))
+                players[i].append([players[i+median][0]])
+                players[i+median].append([players[i][0]])
+        else:
+            pass
+        return versus
+
     def test(self):
-        print(self._players)
+        print(f"Liste des joueurs : {self._tournament.players}")
+        players = self.sort_players()
+        print(f"Liste triée {players}")
+        vs = self.generate_versus()
+        print(f"Versus : {vs}")
